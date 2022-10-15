48bd0134f90e3ad2a5fae5e0abffba30d69e3c7f
OAP-Tima <olutimayinemife@gmail.com>
2022-10-14 19:01:00 -0700

    Second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,12 @@
-#include <stidlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- *  main - Prints a random number and states whether
- *         it is positive, negative, or zero.
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
  *
- *  Return: Always 0.
+ * Return: Always 0.
 */
 int main(void)
 {
