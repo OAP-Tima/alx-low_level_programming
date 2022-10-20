aee06f61e30781cbc0cd27ebea54e69c4df1070c
OAP-Tima <olutimayinemife@gmail.com>
2022-10-19 18:12:15 -0700

    Third commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * main - print_alphabet_x10
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  * Return: Always 0.
  */
@@ -14,5 +15,6 @@ int main(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
+		return (0);
 	}
 }
