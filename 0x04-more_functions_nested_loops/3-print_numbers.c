1fb7bb0f0605c959903e221a44ed0b160a0a1ae6
OAP-Tima <olutimayinemife@gmail.com>
2022-10-21 20:02:45 -0700

    Fourth commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0 to 9 and a new line
+ * print_numbers - prints from 0 to 9
  *
- * Return: void
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
+	int c = 0;
+
 	for (c = '0'; c <= '9'; c++)
 		_putchar(c);
 	_putchar ('\n');
